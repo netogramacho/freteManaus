1876c75e016b1a646a4f921e753e8f399ebb0d7b
Paulo Roberto Gramacho Neto <paulo@Paulos-MacBook-Air.local>
2020-08-05 17:59:59 -0300

    adicionando emails ao rodape

--- rodape.php
+++ rodape.php
@@ -10,11 +10,42 @@
                 </h5>   
             </div>
             <div data-aos="fade">
-                <p>
-                    comercial@triunfocanotagem.com.br
-                    </br>
-                    (19) 98765-4321
-                </p>
+                <div class="row">
+                    <div class="col-md-2"></div>
+                    <div class="col-md-8">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <p>
+                                    comercial@triunfocanotagem.com.br
+                                </p>
+                            </div>
+                            <div class="col-md-4">
+                                <p>
+                                    operacional@triunfocabotagem.com.br
+                                </p>
+                            </div>
+                            <div class="col-md-4">
+                                <p>
+                                    triunfocabotagem@gmail.com
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="offset-md-2"></div>
+                            <div class="col-md-4">
+                                <p>
+                                    (19) 3458-1628
+                                </p>
+                            </div>
+                            <div class="col-md-4">
+                                <p>
+                                    (19) 98870-0830
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-2"></div>
+                </div>
                 <div onclick="voltarTopo()" class="btn btn_rodape">
                     VOLTAR AO TOPO
                 </div>
